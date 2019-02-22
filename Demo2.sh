f3d054f4aa3e32c554407c6df37ce24747feb4c7
Darpan Gupta <darpan.gupta@paytm.com>
2019-02-22 16:23:37 +0530

    file changed

--- Demo2.sh
+++ Demo2.sh
@@ -2,7 +2,4 @@ echo "Enter the month & date like Jan 30"
 read var
 Date=`ls -ltr | awk '{print $6,$7,$9}' | grep -n "$var" | awk -F':' '{print $1}' | head -1`
 Date=`expr $Date - 1`
-ls -ltr | nl | head -$Date | awk '{print $10}'
-echo "Removing above files older than $var days"
 rm -r `ls -ltr | nl | head -$Date | awk '{print $10}'`
-
